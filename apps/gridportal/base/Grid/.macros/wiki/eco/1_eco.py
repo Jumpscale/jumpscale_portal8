409839657c3c43e6064b97fd1413ce280a32ed59
Muhamad Azmy <muhamad.azmy@codescalers.com>
2016-01-10 15:41:08 +0200

    Finished working on the Job page

--- apps/gridportal/base/Grid/.macros/wiki/eco/1_eco.py
+++ apps/gridportal/base/Grid/.macros/wiki/eco/1_eco.py
@@ -1,16 +1,21 @@
 import datetime
 
 def main(j, args, params, tags, tasklet):
-    guid = args.getTag('guid')
-    if not guid:
-        out = 'Missing ECO id param "id"'
+    jid = args.getTag('jid')
+    gid = args.getTag('gid')
+    nid = args.getTag('nid')
+
+    if not jid or not gid or not nid:
+        out = 'Missing ECO params'
         params.result = (out, args.doc)
         return params
 
+    gid = int(gid)
+    nid = int(nid)
     try:
-        obj = j.data.models.system.Errorcondition.get(guid=guid).to_dict()
+        obj = j.data.models.system.Errorcondition.objects.get(__raw__={'jid': jid, 'gid': gid, 'nid': nid}).to_dict()
     except:
-        out = 'Could not find Error Condition Object with guid %s'  % guid
+        out = 'Could not find Error Condition Object for jid: %s, gid: %s and nid: %s' % (jid, gid, nid)
         params.result = (out, args.doc)
         return params
 
@@ -20,7 +25,7 @@ def main(j, args, params, tags, tasklet):
         obj[attr] = j.html.escape(obj[attr])
     for attr in ['jid']:
         obj['jid'] = '[%(jid)s|job?id=%(jid)s]|' % obj if obj[attr] != 0 else 'N/A'
-    obj['guid'] = guid
+    obj['guid'] = jid
 
     args.doc.applyTemplate(obj)
     params.result = (args.doc, args.doc)
--- apps/gridportal/base/Grid/.macros/wiki/job/1_job.py
+++ apps/gridportal/base/Grid/.macros/wiki/job/1_job.py
@@ -3,65 +3,26 @@ import json # pretty printer require native json
 
 def main(j, args, params, tags, tasklet):    
     import urllib
-    id = args.getTag('id')
-    if not id:
+    guid = args.getTag('guid')
+    if not guid:
         out = 'Missing job id param "id"'
         params.result = (out, args.doc)
         return params
 
-    job = j.apps.system.gridmanager.getJobs(guid=id)
-    if not job:
-        params.result = ('Job with id %s not found' % id, args.doc)
+    command = j.data.models.system.Command.get(guid=guid)
+    if not command:
+        params.result = ('Job with id %s not found' % guid, args.doc)
         return params
 
-    node_model = j.data.model.getNodeModel()
+    obj = command.to_dict()
 
-    obj = job[0].to_dict()
-
-    obj['nid'] = obj.get('nid', 0)
+    obj['node'] = {'name': 'N/A'}
     if obj['nid']:
-        obj['node'] = j.data.model.find(node_model,{'nid':obj['nid']})
-    else:
-        obj['node'] = {'name': 'N/A'}
+        node = j.data.models.system.Node.find({'nid': obj['nid'], 'gid': obj['gid']})
+        obj['node'] = node
+
     obj['roles'] = ', '.join(obj['roles'])
-    try:
-        obj['args'] = json.loads(obj['args'])
-        for key, value in obj['args'].items():
-            if isinstance(value, (list, dict)):
-                obj['args'][key] = json.dumps(value, indent=4, sort_keys=True)
-
-    except:
-        obj['args'] = {}
-
-    if obj["state"] == "ERROR":
-        obj['state'] = "FAILED"
-        try:
-            eco = json.loads(obj['result'])
-            if j.core.portal.active.osis.exists('system', 'eco', eco['guid']):
-                obj['resultline'] = '{{errorresult ecoguid:%s}}' % eco['guid']
-            else:
-                obj['resultline'] = "ECO: Is not available anymore"
-            obj['backtrace'] = eco['backtrace']
-        except Exception:
-            eco = {'errormessage': obj['result']}
-        obj['result'] = eco['errormessage'].replace('\n', '$LF')
-    else:
-        try:
-            result = json.loads(obj['result'])
-        except:
-            result = obj['result']
-        try:
-            result = json.dumps(result, indent=4, sort_keys=True)
-        except:
-            pass # so no pretty json then
-        obj['result'] = j.html.escape(result)
-        obj['resultline'] = '{{successfulresult result:%s}}' % urllib.quote(obj['result'])
-
-    if '/' in obj['cmd']:
-        obj['organization'], obj['cmd'] = obj['cmd'].split('/')
-    else:
-        obj['organization'] = obj['category']
-   
+    obj['args'] = j.data.serializer.json.dumps(command.args, indent=2)
 
     args.doc.applyTemplate(obj)
 
