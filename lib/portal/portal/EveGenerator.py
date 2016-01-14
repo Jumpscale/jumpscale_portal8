fd120177e6a3c7f91a13842d3b6a32d2c4475ad6
Reem Khamis <khamisr@codescalers.com>
2016-01-14 16:07:51 +0200

    Eve to be part of portal server Example usage in /grid/Error Conditions_tmp Eve authentication added

---
+++ lib/portal/portal/EveGenerator.py
@@ -0,0 +1,22 @@
+from JumpScale import j
+import eve_mongoengine
+
+
+def generateDomain(models):
+    domain = dict()
+    specs = models._modelspecs
+    for modelname, modelspec in specs.items():
+        domain[modelname.lower()] = generateModel(modelspec)
+    return domain
+
+
+def generateModel(modelspec):
+    model = {'item_url': 'regex("[a-f0-9]+")',
+             'item_lookup_field': '_id',
+             'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
+             'resource_methods': ['GET', 'POST', 'DELETE'],
+             'allowed_roles': ['admin'],
+             'allowed_item_roles':['admin'],
+             'url': modelspec._class_name.lower(),
+             'schema': eve_mongoengine.schema.SchemaMapper.create_schema(modelspec)}
+    return model
