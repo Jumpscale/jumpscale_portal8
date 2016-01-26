d76a2776b72dc97c8ea54939475e77df81aae050
Nourhan Omar <omarn@codescalers.com>
2016-01-26 09:06:09 +0000

    Converting object to dict

--- apps/portalbase/macros/wiki/group_info/5_groupinfo.py
+++ apps/portalbase/macros/wiki/group_info/5_groupinfo.py
@@ -10,9 +10,8 @@ def main(j, args, params, tags, tasklet):
         out = 'Could not find Group: %s' % guid
         params.result = (out, args.doc)
         return params
-    group.to_dict()
 
-    obj = group
+    obj = group.to_dict()
     args.doc.applyTemplate(obj)
     params.result = (args.doc, args.doc)
 
