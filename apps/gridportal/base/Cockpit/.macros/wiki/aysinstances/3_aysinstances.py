a2ceccb4287883dee5e6b222c6e0dc5459d57b6a
Christophe de Carvalho <zaibon@users.noreply.github.com>
2016-06-13 16:42:35 +0200

    Use grid macro instead of simple list (#64)
    * show files in code block + add explorer for service instance
    
    * use grid macro to display instances and templates

--- apps/gridportal/base/Cockpit/.macros/wiki/aysinstances/3_aysinstances.py
+++ apps/gridportal/base/Cockpit/.macros/wiki/aysinstances/3_aysinstances.py
@@ -2,16 +2,31 @@
 def main(j, args, params, tags, tasklet):
     doc = args.doc
     ayspath = args.getTag('ayspath')
+    params.merge(args)
+    out = []
 
-    out = list()
+    actor = j.apps.actorsloader.getActor("system", "atyourservice")
+
+    # this makes sure bootstrap datatables functionality is used
+    out.append("{{datatables_use}}\n")
+
+    fields = ['Role', 'Instance']
     out.append('||Role||Instance||')
-    for ayspath, services in j.apps.system.atyourservice.listServices(ayspath).items():
-        for service in services.values():
-            out.append('|%s|[%s|cockpit/AYSInstance?shortkey=%s&ayspath=%s]|' % (service.role,
-                                                                                 service.instance,
-                                                                                 service.key,
-                                                                                 ayspath))
-    out = '\n'.join(out)
-    params.result = (out, doc)
 
+    for ayspath, services in actor.listServices(ayspath).items():
+        services = sorted(services.values(), key=lambda service: service.role)
+        for service in services:
+            line = [""]
+            for field in fields:
+                if field.lower() == 'instance':
+                    line.append('[%s|cockpit/AYSInstance?shortkey=%s&ayspath=%s]' % (service.instance,
+                                                                                     service.key,
+                                                                                     ayspath))
+                else:
+                    line.append(service.role)
+
+            line.append("")
+            out.append("|".join(line))
+
+    params.result = ('\n'.join(out), doc)
     return params
