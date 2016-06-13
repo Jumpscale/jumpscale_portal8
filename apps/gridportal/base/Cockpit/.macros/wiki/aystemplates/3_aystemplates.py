02e9e2d5ec38d9cd0c2f6e2da0d416388bd341df
christophe dCPM <christophe@incubaid.com>
2016-06-13 14:34:00 +0000

    use grid macro to display instances and templates

--- apps/gridportal/base/Cockpit/.macros/wiki/aystemplates/3_aystemplates.py
+++ apps/gridportal/base/Cockpit/.macros/wiki/aystemplates/3_aystemplates.py
@@ -4,13 +4,21 @@ def main(j, args, params, tags, tasklet):
     ayspath = args.getTag('ayspath')
     out = list()
 
+    # this makes sure bootstrap datatables functionality is used
+    out.append("{{datatables_use}}\n")
+    fields = ['Name']
+
     for ayspath, templates in j.apps.system.atyourservice.listTemplates(ayspath).items():
         out.append('h5. AYSRepo: %s' % ayspath)
         out.append('||Name||')
 
-        for template in templates.values():
-            out.append('|[%s|cockpit/AYSTemplate?ayspath=%s&aysname=%s]|' % (template.name,
-                                                                            ayspath, template.name))
+        templates = sorted(templates.values(), key=lambda template: template.name)
+        for template in templates:
+            line = [""]
+            line.append('[%s|cockpit/AYSTemplate?ayspath=%s&aysname=%s]|' % (template.name,
+                                                                             ayspath, template.name))
+            out.append("|".join(line))
+
     out = '\n'.join(out)
     params.result = (out, doc)
 
