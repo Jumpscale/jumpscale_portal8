3e6a4c4d625a1b782c8b4ca969fa54be7ffe5851
Reem Khamis <khamisr@codescalers.com>
2016-10-10 11:23:26 +0200

    visualizing cProfile of AYS jobs

--- apps/portalbase/AYS81/.macros/page/aysjobprofile/3_aysjobprofile.py
+++ apps/portalbase/AYS81/.macros/page/aysjobprofile/3_aysjobprofile.py
@@ -1,11 +1,14 @@
 
-
 def main(j, args, params, tags, tasklet):
+    page = args.page
+
+    from subprocess import PIPE, Popen
+
     jobid = args.getTag('jobid')
 
     job = j.core.jobcontroller.db.job.get(jobid)
 
-    #### TODO: MOVE INTO ACTOR
+    # TODO: MOVE INTO ACTOR
     # running_viewers = j.sal.process.getPidsByFilter('snakeviz')
     # if len(running_viewers) > 50: # To ensure we don't get overloaded
     #     # kill oldest
@@ -15,10 +18,17 @@ def main(j, args, params, tags, tasklet):
 
     job = job.objectGet()
     j.sal.fs.writeFile(filename=stats, contents=job.model.dbobj.profileData, append=False)
-    cmd = "snakeviz -s %s" % stats
-    link = j.tools.cuisine.local.core.run(cmd)
 
-    args.doc.applyTemplate({'link': link})
-    params.result = (args.doc, args.doc)
+    p = Popen(['python', '-u', '/usr/local/bin/snakeviz', '-s', stats], stdout=PIPE)
+
+    line = p.stdout.readline().decode()
+    while 'http' not in line:
+        line = p.stdout.readline()
+        line = line.decode()
+        print(line)
+
+    page.addHTML("<iframe src=%s frameborder='0' width=100%% height=600 position=absolute></iframe>" % line)
+
+    params.result = page
 
     return params
