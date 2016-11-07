e0ab6f705ce7461a2cc4d8c1601f83dd2f2fb106
Ahmed T. Youssef <xmonader@gmail.com>
2016-11-07 16:12:17 +0200

    timeout and keeping the latest 50

--- apps/portalbase/AYS81/.macros/page/aysjobprofile/3_aysjobprofile.py
+++ apps/portalbase/AYS81/.macros/page/aysjobprofile/3_aysjobprofile.py
@@ -6,26 +6,27 @@ def main(j, args, params, tags, tasklet):
 
     jobid = args.getTag('jobid')
 
-    job = j.core.jobcontroller.db.job.get(jobid)
-
-    # TODO: MOVE INTO ACTOR
-    # running_viewers = j.sal.process.getPidsByFilter('snakeviz')
-    # if len(running_viewers) > 50: # To ensure we don't get overloaded
-    #     # kill oldest
-    #     pass
+    job = j.core.jobcontroller.db.jobs.get(jobid)
 
     stats = j.sal.fs.getTempFileName()
 
+    running_viewers = j.sal.process.getPidsByFilterSortable('snakeviz', 'start_time')
+    if len(running_viewers) > 50: # To ensure we don't get overloaded
+        # kill oldest
+        procstokeep = set(running_viewers[-50:-1])
+        procstoremove = set(running_viewers).symmetric_difference(procstokeep)
+        for p in procstoremove:
+            j.sal.process.kill(p)
     job = job.objectGet()
     j.sal.fs.writeFile(filename=stats, contents=job.model.dbobj.profileData, append=False)
 
-    p = Popen(['python', '-u', '/usr/local/bin/snakeviz', '-s', stats], stdout=PIPE)
+    p = Popen(['timeout', '1m', 'python3', '-u', '/usr/local/bin/snakeviz', '-s', stats], stdout=PIPE)
 
     line = p.stdout.readline().decode()
     while 'http' not in line:
         line = p.stdout.readline()
         line = line.decode()
-        print(line)
+        #print(line)
 
     page.addHTML("<iframe src=%s frameborder='0' width=100%% height=600 position=absolute></iframe>" % line)
 
