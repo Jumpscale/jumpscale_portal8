ebb34be56ee4e3de40d318e8418d555c8c85eae9
Ashraf Fouda <foudaa@codescalers.com>
2016-11-29 11:50:11 +0200

    Adds repo version page Resolves: https://github.com/Jumpscale/jscockpit/issues/105
    Signed-off-by: Ashraf Fouda <foudaa@codescalers.com>

---
+++ apps/portalbase/AYS81/.macros/wiki/repoversion/3_repoversion.py
@@ -0,0 +1,16 @@
+def main(j, args, params, tags, tasklet):
+    doc = args.doc
+    out = []
+    account = tags.tagGet('account')
+    repo = tags.tagGet('repo')
+    provider = tags.tagGet('provider')
+
+    path = j.do.getGitReposListLocal(provider, account, repo)[repo]
+
+    ref = j.clients.git.get(path).getBranchOrTag()
+    out.append(ref[1])
+
+    out = '\n'.join(out)
+
+    params.result = (ref[1], doc)
+    return params
