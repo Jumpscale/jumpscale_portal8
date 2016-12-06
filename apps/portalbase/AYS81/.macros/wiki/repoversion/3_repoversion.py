75090fffc44e15add0875c9a050abfde582215cf
Ashraf Fouda <foudaa@greenitglobe.com>
2016-12-06 10:37:55 +0200

    Adds get the versions from flist if exists
    Signed-off-by: Ashraf Fouda <foudaa@greenitglobe.com>

--- apps/portalbase/AYS81/.macros/wiki/repoversion/3_repoversion.py
+++ apps/portalbase/AYS81/.macros/wiki/repoversion/3_repoversion.py
@@ -5,12 +5,29 @@ def main(j, args, params, tags, tasklet):
     repo = tags.tagGet('repo')
     provider = tags.tagGet('provider')
 
-    path = j.do.getGitReposListLocal(provider, account, repo).get(repo)
-    if path:
-        ref = j.clients.git.get(path).getBranchOrTag()
-        out.append(ref[1])
+    # First try to get the version/branch from the build.xml if it is exists
+    # this mean you are on a mounted file system
+    # if not get them from the repo itself
+
+    cuisine = j.tools.cuisine.local
+    cfg_path = cuisine.core.args_replace("$optDir/build.yaml")
+    if cuisine.core.file_exists(cfg_path):
+        config = j.data.serializer.yaml.loads(cuisine.core.file_read(cfg_path))
+        if repo == 'jumpscale_core8':
+            repo = 'jumpscale'
+        elif repo == 'jumpscale_portal8':
+            repo = 'portal'
+        elif repo == 'jscockpit':
+            repo = 'cockpit'
+        if repo in config:
+            out.append(config[repo])
+    else:
+        path = j.do.getGitReposListLocal(provider, account, repo).get(repo)
+        if path
+            ref = j.clients.git.get(path).getBranchOrTag()
+            out.append(ref[1])
 
     out = '\n'.join(out)
 
-    params.result = (ref[1], doc)
+    params.result = (out, doc)
     return params
