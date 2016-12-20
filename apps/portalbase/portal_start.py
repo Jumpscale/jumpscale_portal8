f447fb77fc14393a1587bd15338ed7b170b8a751
despiegk <kristof@incubaid.com>
2016-12-20 06:03:27 +0100

    dir fixes

--- apps/portalbase/portal_start.py
+++ apps/portalbase/portal_start.py
@@ -23,7 +23,7 @@ def cli(ctx, instance):
 @click.option('--instance', default='main', help='instance of portal')
 def start(ctx, instance):
     instance = instance or ctx.obj.get('INSTANCE')
-    hrd = j.data.hrd.get('%s/portals/%s/config.hrd' % (j.dirs.cfgDir, instance))
+    hrd = j.data.hrd.get('%s/portals/%s/config.hrd' % (j.dirs.CFGDIR, instance))
     j.application.instanceconfig = hrd
 
     j.application.start("portal")
