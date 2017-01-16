6700847ae3e324293658e4fb6a0d148e881f1150
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-01-16 17:55:40 +0100

    don't erase variable in loop
    fixes https://github.com/Jumpscale/jscockpit/issues/348

--- apps/portalbase/AYS81/.macros/wiki/aysservice/3_aysservice.py
+++ apps/portalbase/AYS81/.macros/wiki/aysservice/3_aysservice.py
@@ -2,55 +2,60 @@ from collections import OrderedDict
 
 
 def main(j, args, params, tags, tasklet):
-    role = args.getTag('aysrole')
-    name = args.getTag('aysname')
-    ayspath = args.getTag('ayspath') or ''
+    try:
+        role = args.getTag('aysrole')
+        name = args.getTag('aysname')
+        ayspath = args.getTag('ayspath') or ''
 
-    repo = j.atyourservice.repoGet(ayspath)
-    service = repo.serviceGet(role, name, die=False)
-    if service:
-        prods = {}
-        for role, producers in service.producers.items():
-            prods.setdefault(role, [])
-            for producer in producers:
-                prods[role].append('[{name}|/ays81/Service?aysrole={role}&aysname={name}&ayspath={path}]'.format(
-                    role=role, path=ayspath, name=producer.model.dbobj.name))
+        repo = j.atyourservice.repoGet(ayspath)
+        service = repo.serviceGet(role, name, die=False)
+        if service:
+            prods = {}
+            for prod_role, producers in service.producers.items():
+                prods.setdefault(prod_role, [])
+                for producer in producers:
+                    prods[prod_role].append('[{name}|/ays81/Service?aysrole={role}&aysname={name}&ayspath={path}]'.format(
+                        role=prod_role, path=ayspath, name=producer.model.dbobj.name))
 
-        parent = {}
-        if service.parent is not None:
-            parent['link'] = '[{name}|/ays81/Service?aysrole={role}&aysname={name}&ayspath={path}]'.format(
-                role=service.parent.model.role, path=ayspath, name=service.parent.model.dbobj.name)
+            parent = {}
+            if service.parent is not None:
+                parent['role'] = service.parent.model.role
+                parent['link'] = '[{name}|/ays81/Service?aysrole={role}&aysname={name}&ayspath={path}]'.format(
+                    role=service.parent.model.role, path=ayspath, name=service.parent.model.dbobj.name)
 
-        link_to_template = ('[%s|ays81/ActorTemplate?ayspath=%s&aysname=%s]' % (role,
-                                                                                ayspath, role))
+            link_to_template = ('[%s|ays81/ActorTemplate?ayspath=%s&aysname=%s]' % (role,
+                                                                                    ayspath, role))
 
-        # we prepend service path with '$codedir' to make it work in the explorer.
-        # because of this line :
-        # https://github.com/Jumpscale/jumpscale_portal8/blob/master/apps/portalbase/macros/page/explorer/1_main.py#L25
+            # we prepend service path with '$codedir' to make it work in the explorer.
+            # because of this line :
+            # https://github.com/Jumpscale/jumpscale_portal8/blob/master/apps/portalbase/macros/page/explorer/1_main.py#L25
 
-        hidden = ['key.priv', 'password', 'passwd', 'pwd', 'oauth.jwt_key', 'keyPriv']
-        data = j.data.serializer.json.loads(service.model.dataJSON)
-        data_revised = dict()
-        for k, v in data.items():
-            if k.strip() in hidden:
-                continue
-            else:
-                data_revised[k] = v.replace('\\n', '') if isinstance(v, str) else v
+            hidden = ['key.priv', 'password', 'passwd', 'pwd', 'oauth.jwt_key', 'keyPriv']
+            data = j.data.serializer.json.loads(service.model.dataJSON)
+            data_revised = dict()
+            for k, v in data.items():
+                if k.strip() in hidden:
+                    continue
+                else:
+                    data_revised[k] = v.replace('\\n', '') if isinstance(v, str) else v
 
-        args.doc.applyTemplate({
-            'service': service,
-            'type': link_to_template,
-            'data': data_revised,
-            'name': name,
-            'role': role,
-            'producers': OrderedDict(sorted(prods.items())),
-            'parent': parent,
-            'actions': service.model.actions,
-            'reponame': service.aysrepo.name,
-        })
+            args.doc.applyTemplate({
+                'service': service,
+                'type': link_to_template,
+                'data': data_revised,
+                'name': name,
+                'role': role,
+                'producers': OrderedDict(sorted(prods.items())),
+                'parent': parent,
+                'actions': service.model.actions,
+                'reponame': service.aysrepo.name,
+            })
 
-    else:
-        args.doc.applyTemplate({'error': 'service not found'})
+        else:
+            args.doc.applyTemplate({'error': 'service not found'})
+
+    except Exception as e:
+        args.doc.applyTemplate({'error': e.__str__()})
 
     params.result = (args.doc, args.doc)
     return params
