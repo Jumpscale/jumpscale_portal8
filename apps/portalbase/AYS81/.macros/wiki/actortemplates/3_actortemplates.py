3fadcb815abacd52e32351bef23575925e2e4240
alichaddad <chaddada@greenitglobe.com>
2017-04-18 12:51:40 +0200

    Hardening pages

--- apps/portalbase/AYS81/.macros/wiki/actortemplates/3_actortemplates.py
+++ apps/portalbase/AYS81/.macros/wiki/actortemplates/3_actortemplates.py
@@ -7,12 +7,12 @@ def main(j, args, params, tags, tasklet):
     ctx = args.requestContext
     aysactor = j.apps.actorsloader.getActor('system', 'atyourservice')
     client = aysactor.get_client(ctx=ctx)
-    if reponame:
-        templates = client.listTemplates(repository=reponame).json()
-    else:
-        templates = client.listAYSTemplates().json()
     try:
-        args.doc.applyTemplate({'templates': templates, 'reponame': reponame})
+        if reponame:
+            templates = client.listTemplates(repository=reponame).json()
+        else:
+            templates = client.listAYSTemplates().json()
+            args.doc.applyTemplate({'templates': templates, 'reponame': reponame})
     except Exception as e:
         args.doc.applyTemplate({'error': str(e)})
 
