b60aaa74761d56fcec2ebc5a02c4e662c3635751
Ahmed T. Youssef <xmonader@gmail.com>
2017-04-23 09:35:23 +0200

    removing repoexists macro and using it in Repo.wiki

--- apps/portalbase/AYS81/.macros/wiki/aysrepo/3_aysrepo.py
+++ apps/portalbase/AYS81/.macros/wiki/aysrepo/3_aysrepo.py
@@ -1,10 +1,7 @@
-
+import requests
 
 def main(j, args, params, tags, tasklet):
     try:
-        # arg_repo = args.getTag('reponame')
-        # reponame = j.sal.fs.getBaseName(arg_repo)
-
         ctx = args.requestContext
         aysactor = j.apps.actorsloader.getActor('system', 'atyourservice')
         client = aysactor.get_client(ctx=ctx)
@@ -14,7 +11,12 @@ def main(j, args, params, tags, tasklet):
         args.doc.applyTemplate({'repo': repo})
         params.result = (args.doc, args.doc)
 
+    except requests.exceptions.HTTPError as e:
+        err = "%s" % e.response.json()['error']
+        args.doc.applyTemplate({'error': err})
+        params.result = (args.doc, args.doc)
+
     except Exception as e:
-        args.doc.applyTemplate({'error': e.__str__()})
+        args.doc.applyTemplate({'error': str(e)})
         params.result = (args.doc, args.doc)
     return params
